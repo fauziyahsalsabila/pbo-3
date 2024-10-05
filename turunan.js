d9bbd0aec84b8a20a00a99ace0ea623de0dc051b
fauziyahsalsabila <fauziyah03@upi.edu>
2024-10-05 22:16:54 +0700

    Add files via upload

--- turunan.js
+++ turunan.js
@@ -1,47 +1,47 @@
 class Kapal {
-    constructor(nama, jenis, panjang, lebar) {
-      this.nama = nama;
-      this.jenis = jenis;
-      this.panjang = panjang;
-      this.lebar = lebar;
-    }
-    infoKapal() {
-      return `${this.nama} memiliki jenis kapal ${this.jenis} yang berukuran ${this.panjang} x ${this.lebar} m.`;
-    }
+  constructor(nama, jenis, panjang, lebar) {
+    this.nama = nama;
+    this.jenis = jenis;
+    this.panjang = panjang;
+    this.lebar = lebar;
   }
-  class kapalPenumpaang extends Kapal{
-    constructor(nama, jenis, panjang, lebar, kapasitasPenumpang) {
-      super(nama, jenis, panjang, lebar);
-      this.kapasitasPenumpang = kapasitasPenumpang; 
-    }
-    infoKapal() {
-      return `${super.infoKapal()} Kapal ini memiliki kapasitas ${this.kapasitasPenumpang} orang.`;
-    }
+  infoKapal() {
+    return `${this.nama} memiliki jenis kapal ${this.jenis} yang berukuran ${this.panjang} x ${this.lebar} m.`;
   }
-  //turunan subclass//
-  class kecepatanKapal extends kapalPenumpaang {
-    constructor(nama, jenis, panjang, lebar, kapasitasPenumpang, kecepatanmax){
-        super(nama, jenis, panjang, lebar, kapasitasPenumpang);
-        this.kecepatanmax = kecepatanmax;
-    }
-    infoKapal() {
-        return `${super.infoKapal()} Kapal ini memiliki kecepatan maksimum ${this.kecepatanmax} knot.`;
-      }
-    }
-    //turunan superclass//
-    class kerusakanKapal extends Kapal {
-        constructor (nama, jenis, panjang, lebar, rusak){
-            super(nama, jenis, panjang, lebar);
-            this.rusak = rusak;
-        }
-        infoKapal(){
-            return`${super.infoKapal()} Kapal ini mengalami kerusakan bagian ${this.rusak}.`;
-        }
-    }
+}
+class kapalPenumpaang extends Kapal {
+  constructor(nama, jenis, panjang, lebar, kapasitasPenumpang) {
+    super(nama, jenis, panjang, lebar);
+    this.kapasitasPenumpang = kapasitasPenumpang;
+  }
+  infoKapal() {
+    return `${super.infoKapal()} Kapal ini memiliki kapasitas ${this.kapasitasPenumpang} orang.`;
+  }
+}
+//turunan subclass//
+class kecepatanKapal extends kapalPenumpaang {
+  constructor(nama, jenis, panjang, lebar, kapasitasPenumpang, kecepatanmax) {
+    super(nama, jenis, panjang, lebar, kapasitasPenumpang);
+    this.kecepatanmax = kecepatanmax;
+  }
+  infoKapal() {
+    return `${super.infoKapal()} Kapal ini memiliki kecepatan maksimum ${this.kecepatanmax} knot.`;
+  }
+}
+//turunan subclass//
+class kerusakanKapal extends kecepatanKapal {
+  constructor(nama, jenis, panjang, lebar, kapasitasPenumpang, kecepatanmax, rusak) {
+    super(nama, jenis, panjang, lebar, kapasitasPenumpang, kecepatanmax);
+    this.rusak = rusak;
+  }
+  infoKapal() {
+    return `${super.infoKapal()} Kapal ini mengalami kerusakan pada bagian ${this.rusak}.`;
+  }
+}
 //const kapalFery = new kapalPenumpaang("Budi Ono", "Fery", 200, 100, 500, 70);
 
-const kapalFery = new kecepatanKapal("Budi Ono", "Fery", 200, 100, 500, 70);
+//const kapalFery = new kecepatanKapal("Budi Ono", "Fery", 200, 100, 500, 70);
 
-//const kapalFery = new kerusakanKapal("Budi Ono", "Fery", 200, 100, "jangkar");
+const kapalFery = new kerusakanKapal("Budi Ono", "Fery", 200, 100, 500, 70, "jangkar");
 console.log(kapalFery.infoKapal());
-  
+
